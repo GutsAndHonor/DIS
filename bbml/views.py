020139c4aa9e7836fee452bfe72180c8969fd11f
Kanishk Dogra <Kanishmain@gmail.com>
2023-10-08 14:02:35 -0400

    view

--- bbml/views.py
+++ bbml/views.py
@@ -2,6 +2,14 @@ from django.http import JsonResponse
 from .models import Prediction
 from .serializers import PredictionSerializer
 
+import requests;
+
+def data():
+    url = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions&units=english"
+    response = requests.get(url);
+    data = response.json()["stations"]
+    print(data)
+
 def getPredictions(request):
     predictions = Prediction.objects.all()
     serializer = PredictionSerializer(predictions, many=True)
